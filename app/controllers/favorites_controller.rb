6dc6209b9f2e7f496ec20d3930ba728e0659e97f
yamazakiren1118 <yamazakiren1118@yahoo.co.jp>
2021-03-25 19:04:11 +0900

    tech

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,27 +1,31 @@
 class FavoritesController < ApplicationController
+
   def create
     @favorite = Favorite.new
     @favorite.haiku_id = params[:haiku_id]
     @favorite.user_id = carrent_user.id
+    @haiku = Haiku.find_by(id: params[:haiku_id])
     if @favorite.save
-      flash[:info] = 'いいねしました'
-      redirect_to pages_path
+      flash.now[:info] = 'いいねしました'
+      # render template: 'pages/index'
+      # redirect_to pages_path
     else
       flash[:danger] = 'いいねできませんでした'
       redirect_to pages_path
     end
-    binding.pry
+    # binding.pry
   end
 
   def destroy
     @favorite = Favorite.find_by(haiku_id: params[:haiku_id],user_id: carrent_user.id)
+    @haiku = Haiku.find_by(id: params[:haiku_id])
     if @favorite.delete
       flash[:info] = 'いいねを削除しました'
-      redirect_to pages_path
+      # redirect_to pages_path
     else
       flash[:danger] = 'いいねを削除できませんでした'
-      redirect_to pages_path
+      # redirect_to pages_path
     end
-    binding.pry
+    # binding.pry
   end
 end
